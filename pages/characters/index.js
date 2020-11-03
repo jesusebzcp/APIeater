528b2bc6ba17ee8585da294c295423129012ccef
jesus <jesusebzcp19@gmail.com>
2020-11-03 15:56:39 -0500

    update archive text js

--- pages/characters/index.js
+++ pages/characters/index.js
@@ -1,69 +1,39 @@
-import Link from 'next/link'
-import React, { Component } from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 import axios from "axios";
- 
 
-/*const ENDPOINT =  'https://rickandmortyapi.com/api/character/'
-export async function getServerSideProps(){
-  const  res  = await fetch(ENDPOINT)
-  const  data  = await res.json();
-  return {
-    props: {
-      data
+const CharactersContainer = () => {
+  const [people, setPeople] = useState([]);
+  const activeApi = async () => {
+    try {
+      const res = await axios.get("https://rickandmortyapi.com/api/character/");
+      const personajes = res.data;
+      console.log(personajes.results);
+      setPeople(personajes.results);
+    } catch (error) {
+      console.log("error ==>", error);
     }
-  }
-}
+  };
 
-export default function CharactersContainer({data}) {
-  console.log('data' , data);
-  const { results = [] } = data;
-  return(
-    <>
-      <h1>Lista de personajes</h1>
-      {results.map(character => {
-        const { id, name , image} = character;
-        return (
-          <>
-          <img src={image} alt={`${name} Thumbnail`} />
-          <p key={id}> {name} </p>
-          </>
-          )
-      })}
-    </>
-  )
-}*/
+  useEffect(() => {
+    activeApi();
+  }, []);
 
-
-const CharactersContainer = ({ personajes, error }) => {
-  if (error) {
-    return <div>An error occured: {error.message}</div>;
-  }
-  console.log(personajes + 'AQUI')
   return (
-    <>
-      <h1>Alguien aqui?</h1>
-      {
-        personajes.lenght >= 1 ? personajes.map(personaje => {
-          return <p>{personaje.name}</p>
-        }) : <h2> Nada por aqui</h2>
-      }
-
-
-     
-    </>
-      
-      
+    <div>
+      <h1>Alguien aquí</h1>
+      {people && people.length > 0 ? (
+        people.map((item) => {
+          return (
+            <Fragment>
+              <h1>{item.name}</h1>
+            </Fragment>
+          );
+        })
+      ) : (
+        <h1>cargando.....</h1>
+      )}
+    </div>
   );
-};
-
-CharactersContainer.getInitialProps = async ctx => {
-  try {
-    const res = await axios.get('https://rickandmortyapi.com/api/character/');
-    const personajes = res.data;
-    return { personajes };
-  } catch (error) {
-    return { error };
-  }
 };
 
 export default CharactersContainer;
